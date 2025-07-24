cb5c40ac14c1ca352c25c60dcde315e22fe86836
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2025-07-24 18:30:00 +0200

    Setup github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
+    
     site: siteConfig.website,
     vite: {
         plugins: [tailwindcss()]
--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,8 +39,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
-    title: 'Dante',
+    website: 'https://bartlomiej-milosz.github.io',
+    title: 'My Personal Blog',
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
