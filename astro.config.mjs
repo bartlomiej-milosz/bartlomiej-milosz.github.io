eff1744144aed08c39b96575d5594ceab20999d4
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2025-07-26 12:02:18 +0200

    Style code blocks for both light and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-import siteConfig from './src/data/site-config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import siteConfig from "./src/data/site-config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [mdx(), sitemap()]
+	site: siteConfig.website,
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "vitesse-light",
+				dark: "vitesse-dark",
+			},
+		},
+	},
 });
