230ae0030c06ab08a658f50f34edd65421e08208
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2025-08-04 22:48:49 +0200

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,11 +22,7 @@ export default defineConfig({
 	},
 	integrations: [
 		mdx(), 
-		sitemap({
-			changefreq: 'weekly',
-			priority: 0.7,
-			lastmod: new Date(),
-		})
+		sitemap()
 	],
 	markdown: {
 		shikiConfig: {
